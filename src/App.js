145b917b681bbc24fb23edb93117a795c953b413
Yuranuch <yura477573@mail.ru>
2019-11-15 23:38:19 +0300

    first markup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import UserDatta from "./components/UserData/UserData";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="payForm">
+        <div  className="payForm_title">
+            <span className="payForm_title_totalPay">К оплате: <span>270 BYN</span></span><br/>
+            <span className="payForm_title_info">Годовая подписка на сервис</span>
+        </div>
+
+        <UserDatta />
     </div>
   );
 }
---
+++ src/components/UserData/UserData.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function UserDatta() {
+    return (
+        <div className="userData_wrap">
+            <div>
+                <span>Ваш email</span>
+                <input type="text" name="" value=""/>
+            </div>
+            <div>
+                <span>Номер телефона</span>
+                <input type="text" name="" value=""/>
+            </div>
+        </div>
+    );
+}
+
+export default UserDatta;
