725cda1c8824726a2edef31a2751f44aed644684
Yuranuch <yura477573@mail.ru>
2019-11-18 12:36:32 +0300

    submit error

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Error = ({touched, message}) => {
     if(!touched) {
-        return <div className="form-message invalid">&nbsp;</div>
+        return <div className="form-message invalid"> </div>
     }
     if(message) {
         return <div className="form-message invalid">{message}</div>
     }
-    return <div className="form-message invalid">all good</div>
+    return <div className="form-message invalid"> </div>
 
 }
 
